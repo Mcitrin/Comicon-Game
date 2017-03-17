c165e42f27ac4f79af77fc6be15e49615310db7c
Mike citrin <sickjester86@gmail.com>
2017-03-16 23:36:33 -0700

    super spike Sprites added Ball is disabled Temporarly

---
+++ Comicon Game-git/Assets/scripts/AnimationManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+    public void SetBool(Animator anim, string name, float time)
+    {
+        anim.SetBool(name, true);
+        StartCoroutine(PlayAnimation(anim, name, time));
+    }
+
+    IEnumerator PlayAnimation(Animator anim, string name, float time)
+    {
+        
+        yield return new WaitForSeconds(time);
+        anim.SetBool(name, false);
+    }
+}
--- Comicon Game-git/Assets/scripts/Move.cs
+++ Comicon Game-git/Assets/scripts/Move.cs
@@ -5,9 +5,13 @@ public class Move : MonoBehaviour
 {
 
     InputMan inputMan;
+    AnimationManager animationManager;
     float distToGround;
 
-    public Animator animator;
+    public Animator uperBody;
+    public Animator lowerBody;
+    public AnimationClip smack;
+    public AnimationClip set;
     public int PlayerNumber;
     public GameObject arrow;
     public Vector3 angle;
@@ -16,11 +20,21 @@ public class Move : MonoBehaviour
     public Ball ball;
     float distance = 2;
 
+    bool setting = false;
+    bool chargeing = false;
+    Color[] colors = new Color[3];
+
     // Use this for initialization
     void Awake()
     {
         inputMan = GameManager.gameManager.GetComponent<InputMan>();
+        animationManager = GameManager.gameManager.GetComponent<AnimationManager>();
         distToGround = GetComponent<BoxCollider>().bounds.extents.y;
+
+        
+        colors[0] = gameObject.GetComponentsInChildren<SpriteRenderer>()[0].color;
+        colors[1] = gameObject.GetComponentsInChildren<SpriteRenderer>()[1].color;
+        colors[2] = gameObject.GetComponentsInChildren<SpriteRenderer>()[2].color;
     }
 
     // Update is called once per frame
@@ -28,59 +42,90 @@ public class Move : MonoBehaviour
     {
         Input();
         Animate();
+
+        if (angle.x >= -.5 && angle.x <= .5 && angle.y >= .8)
+        {
+            setting = true;
+        }
+        else
+            setting = false;
+
+        
     }
 
     bool IsGrounded()
     {
-        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.2f);
     }
 
     void Input()
     {
         if (inputMan.Jump(PlayerNumber) && IsGrounded())
         {
-            this.GetComponent<Rigidbody>().velocity = new Vector3(0, 7.5f, 0);//7.5f;
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0, 10.0f, 0);//7.5f;
         }
 
         Vector3 curVel = this.GetComponent<Rigidbody>().velocity;
-        curVel.x = inputMan.Move(PlayerNumber) * 9.0f; // max speed = 5
+        if (!IsGrounded() || chargeing)
+        {
+            curVel.x = inputMan.Move(PlayerNumber) * 2.5f;
+        }
+        else
+        {
+            curVel.x = inputMan.Move(PlayerNumber) * 5.0f;
+        }
+
         this.GetComponent<Rigidbody>().velocity = curVel;
 
         if (inputMan.Aim(PlayerNumber) != Vector2.zero)
         {
             angle = inputMan.Aim(PlayerNumber);
         }
-
-
-
-
-
+        
         arrow.transform.position = angle*1.5f + this.transform.position;
         CalcPower();
-
     }
 
     void Animate()
     {
+        // lowerBody
+        lowerBody.SetBool("Jump", !IsGrounded());
+
         if (inputMan.Move(PlayerNumber) < 0)
         {
-            animator.SetBool("Backward", true);
-            animator.SetBool("Forward", false);
-            animator.SetBool("Still", false);
+            lowerBody.SetBool("Backward", true);
+            lowerBody.SetBool("Forward", false);
+            lowerBody.SetBool("Still", false);
         }
         else if(inputMan.Move(PlayerNumber) > 0)
         {
-            animator.SetBool("Backward", false);
-            animator.SetBool("Forward", true);
-            animator.SetBool("Still", false);
+            lowerBody.SetBool("Backward", false);
+            lowerBody.SetBool("Forward", true);
+            lowerBody.SetBool("Still", false);
         }
         else
         {
-            animator.SetBool("Backward", false);
-            animator.SetBool("Forward", false);
-            animator.SetBool("Still", true);
+            lowerBody.SetBool("Backward", false);
+            lowerBody.SetBool("Forward", false);
+            lowerBody.SetBool("Still", true);
         }
-        animator.SetBool("Jump", !IsGrounded());
+        // uperBody
+
+        if (!setting)
+        {
+            uperBody.SetBool("ChargeSmack", chargeing);
+            uperBody.SetBool("ChargeSet", false);
+        }
+        else
+        {
+            uperBody.SetBool("ChargeSet", chargeing);
+            uperBody.SetBool("ChargeSmack", false);
+        }
+
+        uperBody.SetBool("Still", !chargeing);
+
+
+
     }
 
     void CalcPower()
@@ -89,13 +134,21 @@ public class Move : MonoBehaviour
         {
             if (power == 0)
                 power = Time.time;
+            chargeTime = (Time.time - power);
         }
+
+        if (chargeTime >= .25f)
+        {
+
+            chargeing = true;
+        }
+        else { chargeing = false; }
 
         if (!inputMan.Charge(PlayerNumber))
         {
             if (power != 0)
             {
-                chargeTime = (int)(Time.time - power);
+                
                 if (chargeTime >= 1)
                 {
                     power = 2;
@@ -112,8 +165,16 @@ public class Move : MonoBehaviour
                     ball.HitBall((int)power, angle, PlayerNumber);
                 }
 
-                
-
+                if (!setting)
+                {
+                    animationManager.SetBool(uperBody, "Smack", smack.length);
+                    uperBody.SetBool("Set", false);
+                }
+                else
+                {
+                    animationManager.SetBool(uperBody, "Set", set.length);
+                    uperBody.SetBool("Smack", false);
+                }
                 chargeTime = 0;
                 power = 0;
             }
@@ -122,12 +183,15 @@ public class Move : MonoBehaviour
 
     IEnumerator Flash()
     {
-        Color color = this.gameObject.GetComponent<SpriteRenderer>().color;
         for (int i = 0; i < 8; i++)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            gameObject.GetComponentsInChildren<SpriteRenderer>()[0].color = Color.red;
+            gameObject.GetComponentsInChildren<SpriteRenderer>()[1].color = Color.red;
+            gameObject.GetComponentsInChildren<SpriteRenderer>()[2].color = Color.red;
             yield return new WaitForSeconds(.025f);
-            gameObject.GetComponent<SpriteRenderer>().color = color;
+            gameObject.GetComponentsInChildren<SpriteRenderer>()[0].color = colors[0];
+            gameObject.GetComponentsInChildren<SpriteRenderer>()[1].color = colors[1];
+            gameObject.GetComponentsInChildren<SpriteRenderer>()[2].color = colors[2];
             yield return new WaitForSeconds(.025f);
         }
     }
